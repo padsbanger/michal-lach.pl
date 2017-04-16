09e05bc9b9033982d6f4a8ac00e609bced6f16ed
Michał Lach <kontakt@michal-lach.pl>
2017-04-16 18:27:53 +0200

    more cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,4 @@
-$(function(moment) {
+$(function() {
   function calculateDuration(start, end, unit) {
     var start = moment(start);
     var end = moment(end || new Date());
@@ -13,9 +13,6 @@ $(function(moment) {
       $pixlab = $('.pixlab'),
       $blueoceanmedia = $('.blueoceanmedia'),
       $pjwstk = $('.pjwstk');
-
-    console.log($pjwstk.html())
-
     $gratka.html("<span>"+ calculateDuration('2012-05-01','2012-09-01', 'months') + "</span> months");
     $schibsted.html("<span>"+ calculateDuration('2016-06-01', null , 'months') + "</span> months");
     $epam.html("<span>"+ calculateDuration('2015-05-01', '2016-06-01' , 'months') + "</span> months");
@@ -23,9 +20,6 @@ $(function(moment) {
     $blueoceanmedia.html("<span>"+ calculateDuration('2013-06-01', '2013-08-01' , 'months') + "</span> months");
     $misys.html("<span>"+ calculateDuration('2013-08-01', '2015-04-01' , 'months') + "</span> months");
     $pjwstk.html("<span>"+ calculateDuration('2009-09-01', '2013-02-01' , 'years') + "</span> years");
-
-    console.log($pjwstk.html())
   }
-
   generateDynamicDates()
-}(moment));
+});
